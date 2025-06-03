b8785d910da2b24ef9f816a85662f9b731576b29
Fabien <58579315+formerCIAjanitor@users.noreply.github.com>
2025-06-03 13:42:08 +0900

    edit valid jump chars

--- src/controller.ts
+++ src/controller.ts
@@ -7,7 +7,7 @@ import { invariant } from "./utils";
 
 class Controller {
 	static generateValidJumpChars: () => string[] = () => [
-		..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		..."netsriacpfoyuldhwqNETSRIACPFOYULDHWQ",
 	];
 
 	textEditor?: TextEditor;
